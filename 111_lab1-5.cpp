5dba60a18aee62673e496301f8f3f66b1fb90574
arbid2544 <mrarbid.m@ku.th>
2021-02-01 19:04:46 +0700

    Update 111_lab1-5.cpp

--- 111_lab1-5.cpp
+++ 111_lab1-5.cpp
@@ -1,20 +1,21 @@
 #include<stdio.h>
 int main()
 {
-	int n,max=0,i,p=0;
+	int n,i=0;
 	scanf("%d",&n);
 	int N[n];
 	for(i=0;i<n;i++)
 	{
 	  scanf("%d",&N[i]);
 	}	
-	for(i=0;i<n;i++)
-	{
-	  if(max<N[i]);
-	  {
-	    max=N[i];
-	    p=i+1;
-	  }    
+	int max=N[0],p=0;
+    for(i=1;i<n;i++)
+    {
+        if(N[i]>max)
+        {
+            max=N[i];
+            p=i+1;
+        }
     }
 	printf("%d %d",p,max);
 	return 0;  
